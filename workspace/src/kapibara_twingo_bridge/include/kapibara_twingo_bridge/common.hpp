5076607dcc53c3a73fb67bf9fdb2b64b27d8b457
ProjectRobal <patryk1-70@wp.pl>
2026-08-17 20:00:23 +0200

    Removed unecessary packages

--- workspace/src/kapibara_twingo_bridge/include/kapibara_twingo_bridge/common.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define CONFIG_MAX_BUFFER_SIZE 64
-
-#define TWINGO_ID 3
-
-#define TWINGO_RX_ID 4
--- workspace/src/kapibara_vel_saltis_bridge/include/kapibara_vel_saltis_bridge/common.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define CONFIG_MAX_BUFFER_SIZE 64
-
-#define VEL_SALTIS_ID 1
-
-#define VEL_SALTIS_RX_ID 2
-
-#define G (9.81)
